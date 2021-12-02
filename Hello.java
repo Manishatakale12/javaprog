cf099a4b6d20fc16efa40f23a8a96243b8f4c98d
Manisha Takale <manisha.takale@eternussolutions.com>
2021-12-02 16:13:49 +0530

    first commit

---
+++ Hello.java
@@ -0,0 +1,16 @@
+/**
+ * Hello
+ */
+import java.util.*;
+
+public class Hello {
+
+    public static void main(String[] args) {
+
+        Scanner name=new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String na= name.nextLine();
+        System.out.println("User name is "+na);
+
+    }
+}
