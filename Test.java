c5ecb336428a884f01893e5dbd961c415fe96dd9
Manisha Takale <manisha.takale@eternussolutions.com>
2021-12-02 17:59:36 +0530

    ADdtion functionality in the mt/addition branch

---
+++ Test.java
@@ -0,0 +1,11 @@
+public class Test {
+   
+    public static void main(String[] args) {
+        int a=10,b=10,c;
+        c=a+b;  
+        System.out.println("Addtion of"+a+"and" +b+" is "+c) ;
+    }
+    
+
+    
+}
